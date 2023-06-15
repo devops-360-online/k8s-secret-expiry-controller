535ff3cc556b0d310d35adcda0099187f9178c7d
NLadib-PartnerAuchan <nladib@partner.auchan.com>
2023-06-15 23:18:43 +0200

    feat(api): update SecretWithExpirySpec struct

--- api/v1/secretwithexpiry_types.go
+++ api/v1/secretwithexpiry_types.go
@@ -1,35 +1,13 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // SecretWithExpirySpec defines the desired state of SecretWithExpiry
 type SecretWithExpirySpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of SecretWithExpiry. Edit secretwithexpiry_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	SecretName string     `json:"secretName"`
+	ExpiryDate metav1.Time `json:"expiryDate"`
 }
 
 // SecretWithExpiryStatus defines the observed state of SecretWithExpiry
